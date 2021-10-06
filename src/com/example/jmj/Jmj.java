c64601e54d4ac28513dec82cd6596d418411a19a
jisnamaria <jisnajoji@gmail.com>
2021-10-06 06:57:14 +0530

    second commit

--- src/com/example/jmj/Jmj.java
+++ src/com/example/jmj/Jmj.java
@@ -2,6 +2,9 @@ package com.example.jmj;
 
 public class Jmj {
     public static void main(String[] args) {
-        System.out.println("Jesus MAria Joseph");
+        System.out.println("Jesus Maria Joseph");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
     }
 }
